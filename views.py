eaf35c69447b05232ef2cd3eb9c2b0d0f17957df
Antoine Guenet <aguenet@gmail.com>
2018-02-10 18:57:42 +0100

    Add basic GET and POST for /gifts

--- views.py
+++ views.py
@@ -2,17 +2,17 @@
 
 # For webserver
 from BaseHTTPServer import (BaseHTTPRequestHandler,
-						   HTTPServer)
+                           HTTPServer)
 import cgi  # Common Gateway Interface
 
 # For CRUD
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import (Base,
-					User,
-					Gift,
-					Claim,
-					Category)
+                    User,
+                    Gift,
+                    Claim,
+                    Category)
 
 from flask import (Flask,
                    request,
@@ -37,45 +37,66 @@ app = Flask(__name__)
 # Gifts
 @app.route('/gifts', methods=['GET', 'POST'])
 def cr_gifts():
-	return 'GET or POST gifts'
+    if request.method == 'GET':
+        gifts = c.query(Gift).all()
+
+        html = "<html><body>"
+
+        form = "<form method='POST' action='/gifts'>"
+        form += "<input type='text' name='name' placeholder='name'>"
+        form += "<input type='submit'>"
+        form += "</form>"
+        html += form
+
+        html += "<ul>"
+        for gift in gifts:
+            html += "<li>%s</li>" % gift.name
+        html += "</ul></body></html>"
+        return html
+
+    if request.method == 'POST':
+        gift = Gift(name=request.form['name'])
+        c.add(gift)
+        c.commit()
+        return redirect(url_for('rud_gifts', g_id=gift.id))
 
 
 @app.route('/gifts/<int:g_id>', methods=['GET', 'UPDATE', 'DELETE'])
-def ud_gifts(g_id):
-	return 'GET, UPDATE or DELETE gift %s' % g_id
+def rud_gifts(g_id):
+    return 'GET, UPDATE or DELETE gift %s' % g_id
 
 
 # Claims
 @app.route('/gifts/<int:g_id>/claims', methods=['GET', 'POST'])
 def cr_claims(g_id):
-	return 'GET or POST claims on gift %s' % g_id
+    return 'GET or POST claims on gift %s' % g_id
 
 
 @app.route('/gifts/<int:g_id>/claims/<int:c_id>', methods=['GET', 'UPDATE', 'DELETE'])
-def ud_claims(g_id, c_id):
-	return 'GET, UPDATE or DELETE claim %s on gift %s' % (g_id, c_id)
+def rud_claims(g_id, c_id):
+    return 'GET, UPDATE or DELETE claim %s on gift %s' % (g_id, c_id)
 
 
 # Users
 @app.route('/users', methods=['GET', 'POST'])
 def cr_users():
-	return 'GET or POST users'
+    return 'GET or POST users'
 
 
 @app.route('/users/<int:u_id>', methods=['GET', 'UPDATE', 'DELETE'])
-def ud_users(u_id):
-	return 'GET, UPDATE or DELETE user %s' % u_id
+def rud_users(u_id):
+    return 'GET, UPDATE or DELETE user %s' % u_id
 
 
 # Categories
 @app.route('/categories', methods=['GET', 'POST'])
 def cr_categories():
-	return 'GET or POST categories'
+    return 'GET or POST categories'
 
 
 @app.route('/categories/<int:c_id>', methods=['GET', 'UPDATE', 'DELETE'])
-def ud_categories(c_id):
-	return 'GET, UPDATE or DELETE category %s' % c_id
+def rud_categories(c_id):
+    return 'GET, UPDATE or DELETE category %s' % c_id
 
 
 if __name__ == '__main__':
