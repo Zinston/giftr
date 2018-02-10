f8dee0f2b735b70ffbd7f4282152388864ad4602
Antoine Guenet <aguenet@gmail.com>
2018-02-10 18:00:38 +0100

    Add model for Category.

--- models.py
+++ models.py
@@ -176,6 +176,49 @@ class Claim(Base):
         }
 
 
+# CLASS #
+
+class Category(Base):
+	"""Database table for a gift category."""
+
+	# TABLE #
+	__tablename__ = 'category'
+	# MAPPER#
+	id = Column(
+            Integer,
+            primary_key=True)
+
+	name = Column(
+			String(80),
+			nullable=False)
+
+	description = Column(
+					String(140))
+
+	picture = Column(
+				String(80))
+
+	created_at = Column(
+					Date,
+					default=datetime.datetime.now())
+
+	updated_at = Column(
+					Date,
+					onupdate=datetime.datetime.now())
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format."""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'picture': self.picture,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+
+
 
 # CONFIGURATION #
 
