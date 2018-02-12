017ef48d541bd467a082379ff5c9cfc77d2a6746
Antoine Guenet <aguenet@gmail.com>
2018-02-12 15:58:19 +0100

    Implement Facebook disconnect.

--- views.py
+++ views.py
@@ -689,6 +689,12 @@ def disconnect():
             gdisconnect()
             del session['gplus_id']
 
+        # If they logged in through FACEBOOK
+        if session['provider'] == 'facebook':
+            # Revoke their Facebook access token
+            fbdisconnect()
+            del session['fb_id']
+
         # In any case, log them out...
         session.pop('username', None)
         session.pop('email', None)
@@ -698,7 +704,7 @@ def disconnect():
         session.pop('token', None)
 
         # Flash them
-        flash("You have successfully been logged out.")
+        flash("You have successfully logged out.")
 
         # Redirect them
         return redirect(url_for('get_gifts'))
@@ -753,6 +759,48 @@ def gdisconnect():
     response = make_response(json.dumps('Could not revoke the token.'), 500)
     response.headers['Content-Type'] = 'application/json'
 
+    return response
+
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    # PART 1: Check that there is a user to disconnect
+    # -------
+
+    # Get the ACCESS TOKEN from the session
+    access_token = session.get('access_token')
+
+    # If there is none, then we have no user logged in
+    if access_token is None:
+        response = make_response(json.dumps('Current user not connected'), 401)
+        response.headers['Content-Type'] = 'application/json'
+
+        return response
+
+    # PART 2: Disconnect user
+    # -------
+
+    # Have Facebook revoke the permission
+
+    # 1. Get the URL for FACEBOOK permissions
+    fb_id = session['fb_id']
+    url = 'https://graph.facebook.com/%s/permissions' % fb_id
+
+    # 2. Create a DELETE request with the URL
+    url = 'https://graph.facebook.com/%s/permissions' % fb_id
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+
+    result = requests.delete(url=url, headers=headers)
+
+    # 3. Check if it's a success
+    if result.status_code == '200':
+        # 4. Send the response (DISCONNECT() TAKES CARE OF DELETING SESSION)
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    response = make_response(json.dumps('Could not revoke the token.'), 500)
+    response.headers['Content-Type'] = 'application/json'
     return response
 
 
