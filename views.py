4c7eeb0cb4623d28684081e13d20936ccd2a34fc
Antoine Guenet <aguenet@gmail.com>
2018-02-13 01:26:38 +0100

    Add API endpoints for categories.

--- views.py
+++ views.py
@@ -1003,6 +1003,36 @@ def get_gift_json(g_id):
     return jsonify({'gift': serialized_gift})
 
 
+@app.route('/api/categories')
+def get_categories_json():
+    """Return the gifts in json."""
+    # Query database
+    categories = c.query(Category).all()
+
+    # Serialize
+    serialized_categories = [cat.serialize for cat in categories]
+
+    # Jsonify
+    return jsonify({'categories': serialized_categories})
+
+
+@app.route('/api/categories/<int:cat_id>')
+def get_category_json(cat_id):
+    """Return a category of id cat_id in json.
+
+    Argument:
+    cat_id (int): the desired category.
+    """
+    # Query database
+    category = c.query(Category).filter_by(id=cat_id).first()
+
+    # Serialize
+    serialized_category = category.serialize
+
+    # Jsonify
+    return jsonify({'category': serialized_category})
+
+
 # HELPERS
 
 def create_user_from_session():
