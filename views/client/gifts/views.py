f30d5cdce83f1debeabbb94773700b32841e01f9
Antoine Guenet <aguenet@gmail.com>
2018-02-14 21:24:10 +0100

    Move querying individual gift, checking if user is gift creator and querying all categories into decorators.

--- views/client/gifts/views.py
+++ views/client/gifts/views.py
@@ -42,14 +42,50 @@ def login_required(f):
     return decorated_function
 
 
+def include_gift(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        g_id = kwargs['g_id']
+        gift = c.query(Gift).filter_by(id=g_id).one_or_none()
+        if not gift:
+            flash('There\'s no gift here.')
+            return redirect(url_for('gifts.get'))
+        # pass along the gift object to the next function
+        kwargs['gift'] = gift
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def include_categories(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        categories = c.query(Category).all()
+        # pass along the categories object to the next function
+        kwargs['categories'] = categories
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def creator_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        gift = kwargs['gift']
+
+        if gift.creator_id != session.get('user_id'):
+            flash('You have to be the creator of that gift to see that page.')
+            return redirect(url_for('gifts.get_byid',
+                                    g_id=gift.id))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 # ROUTES
 
 @gifts_blueprint.route('/', methods=['GET'])
 @gifts_blueprint.route('/gifts', methods=['GET'])
-def get():
+@include_categories
+def get(categories):
     """Render all gifts or gifts of category id "cat" if query."""
-    categories = c.query(Category).all()
-
     req_cat = request.args.get('cat')
 
     # If there is a valid int as query string,
@@ -77,7 +113,9 @@ def get():
 
 
 @gifts_blueprint.route('/gifts/<int:g_id>', methods=['GET'])
-def get_byid(g_id):
+@include_gift
+@include_categories
+def get_byid(g_id, gift, categories):
     """Render a gift of id g_id.
 
     Login required.
@@ -85,9 +123,6 @@ def get_byid(g_id):
     Argument:
     g_id (int): the id of the desired gift.
     """
-    gift = c.query(Gift).filter_by(id=g_id).first()
-    categories = c.query(Category).all()
-
     return render_template('gift.html',
                            gift=gift,
                            categories=categories,
@@ -95,11 +130,11 @@ def get_byid(g_id):
 
 
 @gifts_blueprint.route('/gifts/user/<int:u_id>', methods=['GET'])
-def get_byuserid(u_id):
+@include_categories
+def get_byuserid(u_id, categories):
     """Render all gifts created by a user of id u_id."""
     gifts = c.query(Gift).filter_by(creator_id=u_id).order_by(Gift.created_at.desc()).all()
     user = c.query(User).filter_by(id=u_id).first()
-    categories = c.query(Category).all()
 
     return render_template('gifts.html',
                            gifts=gifts,
@@ -110,13 +145,12 @@ def get_byuserid(u_id):
 
 @gifts_blueprint.route('/gifts/add', methods=['GET'])
 @login_required
-def add_get():
+@include_categories
+def add_get(categories):
     """Render form to add a gift.
 
     Login required.
     """
-    categories = c.query(Category).all()
-
     return render_template('add_gift.html',
                            categories=categories)
 
@@ -144,7 +178,10 @@ def add_post():
 
 @gifts_blueprint.route('/gifts/<int:g_id>/edit', methods=['GET'])
 @login_required
-def edit_get(g_id):
+@include_gift
+@creator_required
+@include_categories
+def edit_get(g_id, gift, categories):
     """Render an edit form for a gift of id g_id.
 
     Login required.
@@ -153,15 +190,6 @@ def edit_get(g_id):
     Argument:
     g_id (int): the id of the desired gift.
     """
-    gift = c.query(Gift).filter_by(id=g_id).first()
-
-    if gift.creator_id != session.get('user_id'):
-        flash('You have to be the creator of that gift to see that page.')
-        return redirect(url_for('gifts.get_byid',
-                                g_id=gift.id))
-
-    categories = c.query(Category).all()
-
     return render_template('edit_gift.html',
                            gift=gift,
                            categories=categories)
@@ -169,7 +197,9 @@ def edit_get(g_id):
 
 @gifts_blueprint.route('/gifts/<int:g_id>/edit', methods=['POST'])
 @login_required
-def edit_post(g_id):
+@include_gift
+@creator_required
+def edit_post(g_id, gift):
     """Edit a gift of id g_id with POST.
 
     Login required.
@@ -178,13 +208,6 @@ def edit_post(g_id):
     Argument:
     g_id (int): the id of the desired gift.
     """
-    gift = c.query(Gift).filter_by(id=g_id).first()
-
-    if gift.creator_id != session.get('user_id'):
-        flash('You have to be the creator of that gift to see that page.')
-        return redirect(url_for('gifts.get_byid',
-                                g_id=gift.id))
-
     gift.name = request.form.get('name')
     gift.picture = request.form.get('picture')
     gift.description = request.form.get('description')
@@ -201,7 +224,9 @@ def edit_post(g_id):
 
 @gifts_blueprint.route('/gifts/<int:g_id>/delete', methods=['GET'])
 @login_required
-def delete_get(g_id):
+@include_gift
+@creator_required
+def delete_get(g_id, gift):
     """Render a delete form for a gift of id g_id.
 
     Login required.
@@ -210,20 +235,15 @@ def delete_get(g_id):
     Argument:
     g_id (int): the id of the desired gift.
     """
-    gift = c.query(Gift).filter_by(id=g_id).first()
-
-    if gift.creator_id != session.get('user_id'):
-        flash('You have to be the creator of that gift to see that page.')
-        return redirect(url_for('gifts.get_byid',
-                                g_id=gift.id))
-
     return render_template('delete_gift.html',
                            gift=gift)
 
 
 @gifts_blueprint.route('/gifts/<int:g_id>/delete', methods=['POST'])
 @login_required
-def delete_post(g_id):
+@include_gift
+@creator_required
+def delete_post(g_id, gift):
     """Delete a gift of id g_id with POST.
 
     Login required.
@@ -232,13 +252,6 @@ def delete_post(g_id):
     Argument:
     g_id (int): the id of the desired gift.
     """
-    gift = c.query(Gift).filter_by(id=g_id).first()
-
-    if gift.creator_id != session.get('user_id'):
-        flash('You have to be the creator of that gift to see that page.')
-        return redirect(url_for('gifts.get_byid',
-                                g_id=gift.id))
-
     c.delete(gift)
     c.commit()
 
